180ef06ae36c650f73e41199a4d57ac17c157a75
kirtimadam <104613258+kirtimadam@users.noreply.github.com>
2023-08-16 14:10:00 +0530

    Added : regexp

---
+++ src/main/java/com/bl/RegExp.java
@@ -0,0 +1,19 @@
+package com.bl;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegExp {
+    static Pattern p = Pattern.compile("^[A-Z a-z]{3,}$");
+
+    public static void main(String[] args) {
+        String str = "Kirti";
+        Matcher m = p.matcher(str);
+
+        if(m.matches()){
+            System.out.println("valid");
+        }else {
+            System.out.println("invalid");
+        }
+    }
+}
