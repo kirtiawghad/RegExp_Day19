64cafde2e8d0795b1740aa2479400a5b4d140991
kirtimadam <104613258+kirtimadam@users.noreply.github.com>
2023-08-16 14:13:44 +0530

    added : UC3

--- src/main/java/com/bl/RegExp.java
+++ src/main/java/com/bl/RegExp.java
@@ -4,9 +4,9 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegExp {
-    static Pattern p = Pattern.compile("^[A-Z a-z]{3,}$");
+    static Pattern p = Pattern.compile("^[a-zA-Z0-9][a-zA-Z0-9_.]*[a-zA-Z0-9]@[a-zA-Z]+[.][a-zA-Z]+[.][a-zA-Z]+$");
     public static void main(String[] args) {
-        String str = "Madan";
+        String str = "abc.xyz@bl.co.in";
         Matcher m = p.matcher(str);
         if (m.matches()){
             System.out.println("valid");
