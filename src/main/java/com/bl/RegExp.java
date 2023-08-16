3b268186166624a30d0eb05a748c261c06c75613
kirtimadam <104613258+kirtimadam@users.noreply.github.com>
2023-08-16 14:36:37 +0530

    added : UC5

--- src/main/java/com/bl/RegExp.java
+++ src/main/java/com/bl/RegExp.java
@@ -4,15 +4,16 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegExp {
-    static Pattern p = Pattern.compile("^(0|91)\s?[7-9][0-9]{9}$");
+    static Pattern p = Pattern.compile("^[a-zA-Z][a-zA-Z]{8}$");
     public static void main(String[] args) {
-        String str = "91 9919819801";
-        Matcher m = p.matcher(str);
+        String password = "kirtiMada";
+        Matcher m = p.matcher(password);
         if (m.matches()){
             System.out.println("valid");
         }else {
             System.out.println("invalid");
         }
+
 
     }
 }
