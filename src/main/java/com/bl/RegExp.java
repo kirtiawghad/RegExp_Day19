3e757c11d3dd4fcf2055ad3391ef726eca01988f
kirtimadam <104613258+kirtimadam@users.noreply.github.com>
2023-08-16 14:38:53 +0530

    added : UC6

--- src/main/java/com/bl/RegExp.java
+++ src/main/java/com/bl/RegExp.java
@@ -4,15 +4,16 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegExp {
-    static Pattern p = Pattern.compile("^[a-zA-Z][a-zA-Z]{8}$");
+    static Pattern p = Pattern.compile("^[a-zA-Z][a-zA-Z0-9]*(?=.*[A-Z]).*$");
     public static void main(String[] args) {
-        String password = "kirtiMada";
+        String password = "kirtI";
         Matcher m = p.matcher(password);
         if (m.matches()){
             System.out.println("valid");
         }else {
             System.out.println("invalid");
         }
+
 
 
     }
