7fca3f924741aa2b2cd5cf2b9255c15f7c83dc1f
kirtimadam <104613258+kirtimadam@users.noreply.github.com>
2023-08-16 14:14:54 +0530

    added : UC4

--- src/main/java/com/bl/RegExp.java
+++ src/main/java/com/bl/RegExp.java
@@ -4,16 +4,15 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegExp {
-    static Pattern p = Pattern.compile("^[a-zA-Z0-9][a-zA-Z0-9_.]*[a-zA-Z0-9]@[a-zA-Z]+[.][a-zA-Z]+[.][a-zA-Z]+$");
+    static Pattern p = Pattern.compile("^(0|91)\s?[7-9][0-9]{9}$");
     public static void main(String[] args) {
-        String str = "abc.xyz@bl.co.in";
+        String str = "91 9919819801";
         Matcher m = p.matcher(str);
         if (m.matches()){
             System.out.println("valid");
         }else {
             System.out.println("invalid");
         }
-
 
     }
 }
